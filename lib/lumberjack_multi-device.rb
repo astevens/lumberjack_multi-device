52efca3b97fea7cc5b4c764d5ce0d1220074ed08
misfo <tedwardo2@gmail.com>
2012-04-30 13:57:23 -0500

    Override Device methods

--- lib/lumberjack_multi-device.rb
+++ lib/lumberjack_multi-device.rb
@@ -13,7 +13,7 @@ module Lumberjack
     end
 
     def respond_to?(method, include_private = false)
-      @devices.detect do |device|
+      @devices.all? do |device|
         device.respond_to?(method, include_private)
       end || super
     end
@@ -21,6 +21,24 @@ module Lumberjack
     def method_missing(method, params, &block)
       @devices.each do |device|
         device.send(method, *params, &block)
+      end
+    end
+
+    def write(entry)
+      @devices.map do |device|
+        device.write(entry)
+      end
+    end
+
+    def flush
+      @devices.map do |device|
+        device.flush
+      end
+    end
+
+    def close
+      @devices.map do |device|
+        device.close
       end
     end
 
